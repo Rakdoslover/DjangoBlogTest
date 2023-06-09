3f3b7d4e9806ffcc28465584aa5c27c925414f66
Rakdoslover <>
2023-06-09 15:04:38 +0000

    made updates to the admin page, making it possible to approve comments and make posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-2023-rakdoslover.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-2023-rakdoslover.herokuapp.com', '8000-rakdoslover-djangoblogte-gxearg4wnp.us2.codeanyapp.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
